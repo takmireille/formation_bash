f7378ef2a59b78fbba1e90ea2906c8384038b3a3
takmireille <nzonteumireille@gmail.com>
2023-06-29 07:39:47 -0700

    introduction du scripting dans vscode

---
+++ script_vs.bash/hello_word.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "hello word"
+# creer deux dossiers 
+mkdir Dossier_Compta Dev 2> dev/null
+# creer les fichiers aux sein des differents dossiers
+touch Dossier_Compta/Bilan_2021 2> dev/null
+touch Dossier_Compta/Bilan_2022 2> dev/null
+touch Dev/software1 2> dev/null 
+touch Dev/software2 2> dev/null
