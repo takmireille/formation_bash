73842315e8c28c54c468e6d7e340f41306be54cc
root <root@vm2.myguest.virtualbox.org>
2024-01-07 22:06:26 +0100

    addition argument v1

---
+++ scripts_usuels/add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# arguments
+
+nombres1=$1
+nombre2=$2
+
+if [ "$#" -ne 2 ]; then
+echo " entrez 2 chiffres"
+# addition des arguments
+
+#while [[ "$#" =~ ^[0-9]+$ ]]; do 
+ # result=$($nombre1 + $nombre2)
+ # break
+#done 
+result=$(nombre1 + nombre2)
+echo "la somme des deux nombres est: $result"
