7a721f60fa4f2bd833f0d89d01c785bde673432d
root <root@vm2.myguest.virtualbox.org>
2024-01-07 17:31:25 +0100

    afficher les arguments

---
+++ scripts_usuels/test_arg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+nom_script=$0
+
+nombre_arguments=$#
+
+arg1=$1
+arg2=$2
+
+# afficher les infos
+
+echo "nom du script: $nom_script"
+echo "nombre d'argument: $nombre_arguments"
+echo "les 2 aguments sont: $arg1, $arg2"
