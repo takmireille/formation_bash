4c053ee69d4e9a51027936328f3bd6055ba2b92c
root <root@vm2.myguest.virtualbox.org>
2024-01-08 12:39:57 +0100

    calculation of age whit date of birth entered by user

--- scripts_usuels/age.sh
+++ scripts_usuels/age.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-read -p "entrez votre année de naissance en chiffres:  "
 
-annee_naissance=$#
+echo "veuillez entrer votre année de naissance en chiffre :"
+
+read annee_naissance
+
+#annee_naissance=$(read -p "entrez votre année de naissance en chiffres:  ")
+
+#annee_naissance=$(echo "$1" | awk -F)
 
 annee_actuelle=$(date +%Y)
 
 echo " nous sommes en $annee_actuelle"
+# calcul age
 
-age=$((annee_actuelle - ))
+age=$((annee_actuelle - annee_naissance))
 
-echo " vous avez $age ans"
+echo  "vous avez $age ans"
 
